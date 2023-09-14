3a4707245636a35d2f9104bf07ef046a9f11210d
Hyago Oliveira <hyagogow@gmail.com>
2023-09-14 20:26:45 -0300

    Play rotate audio clip when rotates Ultrahandable object

--- Assets/Scripts/UI/Abilities/Ultrahand.cs
+++ Assets/Scripts/UI/Abilities/Ultrahand.cs
@@ -4,24 +4,37 @@ using TheLegend.Abilities;
 namespace TheLegend.UI
 {
     [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
     public sealed class Ultrahand : AbstractAbility<UltrahandSettings>
     {
         [SerializeField] private Crosshair crosshair;
+        [SerializeField] private AudioSource audioSource;
 
-        private void Reset() => crosshair = GetComponentInChildren<Crosshair>();
+        private void Reset()
+        {
+            crosshair = GetComponentInChildren<Crosshair>();
+            audioSource = GetComponent<AudioSource>();
+        }
+
         private void Awake() => crosshair.Viewer.Hide();
 
         protected override void OnEnable()
         {
             base.OnEnable();
+
+            settings.OnObjectRotated += HandleObjectRotated;
             settings.OnInteractionStarted += HandleObjectInteractionStarted;
         }
 
         protected override void OnDisable()
         {
             base.OnDisable();
+
+            settings.OnObjectRotated -= HandleObjectRotated;
             settings.OnInteractionStarted -= HandleObjectInteractionStarted;
         }
+
+        private void HandleObjectRotated() => audioSource.Play();
 
         private void HandleObjectInteractionStarted(IUltrahandable _) => HandleToggled(false);
 
